00aeef8dc9fad25a6f5b39a314442fb54e518ecb
James Duda <jimbojim1997@gmail.com>
2018-03-07 23:41:48 +0000

    Renamed testing project and added basic merge field template & data

---
+++ NetplateTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using Netplate;
+
+using System;
+using System.Collections.Generic;
+
+namespace NetplateTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string template = @"Dear [Title][~ ][Forename][~ ][Surname],";
+            Console.WriteLine(template);
+            Console.WriteLine("\n----------\n");
+
+            var data = new Dictionary<string, object>()
+            {
+                {"Title", "Mr"},
+                {"Forename", "John"},
+                {"Surname", "Doe"}
+            };
+            string result = MailMerge.Merge(template, data);
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
